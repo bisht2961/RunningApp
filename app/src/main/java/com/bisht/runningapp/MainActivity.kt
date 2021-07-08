866a493c21276c86778399f914f3a32cf1d92d9e
bisht2961 <19bcs2961@gmail.com>
2021-07-08 21:59:25 +0530

    RunningApp

--- app/src/main/java/com/bisht/runningapp/MainActivity.kt
+++ app/src/main/java/com/bisht/runningapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.bisht.runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.bisht.runningapp.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var dao:RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
