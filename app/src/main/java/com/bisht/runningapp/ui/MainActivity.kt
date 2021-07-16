dec19b6cbd90f33abb51c0a6d3e36412dac7ce91
bisht2961 <19bcs2961@gmail.com>
2021-07-16 21:43:28 +0530

    RunningApp

--- app/src/main/java/com/bisht/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/bisht/runningapp/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -25,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         navigateToTrackingFragmentIfNeeded(intent)
         setSupportActionBar(binding.toolbar)
         navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
         navHostFragment.navController
             .addOnDestinationChangedListener{ _, destination, _ ->
@@ -44,7 +47,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun navigateToTrackingFragmentIfNeeded(intent:Intent? ){
         if(intent?.action == ACTION_SHOW_ACTION_FRAGMENT){
-            binding.navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+            navController.navigate(R.id.action_global_trackingFragment)
         }
     }
 }
