ca2899ec652a06d279ff3672adae791f7367b63c
bisht2961 <19bcs2961@gmail.com>
2021-07-16 21:26:15 +0530

    RunningApp

--- app/src/main/java/com/bisht/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/bisht/runningapp/ui/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.bisht.runningapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.bisht.runningapp.R
 import com.bisht.runningapp.databinding.ActivityMainBinding
+import com.bisht.runningapp.other.Constants.ACTION_SHOW_ACTION_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -14,16 +17,17 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navigateToTrackingFragmentIfNeeded(intent)
         setSupportActionBar(binding.toolbar)
-
-        binding.bottomNavigationView.setupWithNavController(binding.navHostFragment.findNavController())
-        binding.navHostFragment.findNavController()
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        navHostFragment.navController
             .addOnDestinationChangedListener{ _, destination, _ ->
                 when(destination.id){
                     R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
@@ -31,5 +35,16 @@ class MainActivity : AppCompatActivity() {
                     else -> binding.bottomNavigationView.visibility = View.GONE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent:Intent? ){
+        if(intent?.action == ACTION_SHOW_ACTION_FRAGMENT){
+            binding.navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
--- app/src/main/java/com/bisht/runningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/bisht/runningapp/ui/fragments/TrackingFragment.kt
@@ -1,14 +1,76 @@
 package com.bisht.runningapp.ui.fragments
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bisht.runningapp.R
+import com.bisht.runningapp.databinding.FragmentSetupBinding
+import com.bisht.runningapp.databinding.FragmentTrackingBinding
+import com.bisht.runningapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.bisht.runningapp.services.TrackingService
 import com.bisht.runningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking){
 
+    private var _binding: FragmentTrackingBinding? = null
+    private val binding get() = _binding!!
     private val viewModel: MainViewModel by viewModels()
+    private var map: GoogleMap? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTrackingBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mapView.onCreate(savedInstanceState)
+        binding.btnToggleRun.setOnClickListener{
+            sendCommandService(ACTION_START_OR_RESUME_SERVICE)
+        }
+        binding.mapView.getMapAsync{
+            map = it
+        }
+        
+    }
+    private fun sendCommandService(action: String) = Intent(requireContext(), TrackingService::class.java).also {
+        it.action = action
+        requireContext().startService(it)
+    }
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
 
 }
