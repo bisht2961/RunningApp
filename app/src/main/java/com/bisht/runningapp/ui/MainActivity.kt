cb419df52ca60f931edb525d1c5c340af6f864fe
bisht2961 <19bcs2961@gmail.com>
2021-07-18 21:30:20 +0530

    RunningApp

--- app/src/main/java/com/bisht/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/bisht/runningapp/ui/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.bisht.runningapp.R
